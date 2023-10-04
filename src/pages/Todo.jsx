103938a311f90d1834c592dd357fc9579dca18a4
Alberto-Corzo <utm20040042@utma.edu.mx>
2023-10-03 20:05:29 -0600

    Agregado nuevos componentes

---
+++ src/pages/Todo.jsx
@@ -0,0 +1,14 @@
+import ListItem from "../components/Lisitem"
+
+function Todo() {
+    return (
+    <div>
+        <ul>
+            <ListItem text={"IMTEL"} />
+        </ul>
+    </div>
+    
+    )
+}
+
+export default Todo
